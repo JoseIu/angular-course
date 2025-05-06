9b29e0a1478556005b3eb33002b415736f0c036a
Joselu <boxixz0991@gmail.com>
2025-05-06 22:54:42 +0200

    feat: add icons

---
+++ gifs-app/src/app/components/search-icon/search-icon.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-icon',
+  imports: [],
+  templateUrl: './search-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchIconComponent { }
---
+++ gifs-app/src/app/components/treding-icon/treding-icon.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'treding-icon',
+  imports: [],
+  templateUrl: './treding-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TredingIconComponent {}

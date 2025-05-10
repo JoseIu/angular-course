d5723ab6b87e261210964a62148952a8db435c73
Joselu <boxixz0991@gmail.com>
2025-05-10 21:30:21 +0200

    feat: eliminar estrategia de detección de cambios en componentes de icono de búsqueda y tendencia

--- gifs-app/src/app/components/search-icon/search-icon.component.ts
+++ gifs-app/src/app/components/search-icon/search-icon.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-icon',
   imports: [],
   templateUrl: './search-icon.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchIconComponent { }
+export class SearchIconComponent {}

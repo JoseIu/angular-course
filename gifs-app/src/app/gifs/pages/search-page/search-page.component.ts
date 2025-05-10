03b171ae2c740a93fe35581c6068a7d17e3d34cf
Joselu <boxixz0991@gmail.com>
2025-05-10 21:31:35 +0200

    feat: implementar la página de búsqueda de Gifs con funcionalidad de búsqueda y visualización

--- gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++ gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GifListSkeletonComponent } from '@app/gifs/components/gif-list-skeleton/gif-list-skeleton.component';
+import { GifsService } from '@app/gifs/services/gif.service';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent, GifListSkeletonComponent],
   templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent {}
+export default class SearchPageComponent {
+  gifsService = inject(GifsService);
+  onSearch(search: string) {
+    if (!search) return;
+    this.gifsService.searchGifs(search).subscribe((gifs) => {
+      this.gifsService.searchGifList.set(gifs);
+      this.gifsService.isSearchingGif.set(false);
+    });
+  }
+}
